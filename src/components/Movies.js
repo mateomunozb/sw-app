bc96ed57bd42cc9eb913107708fda65048feaa3a
root <root@DESKTOP-4I5C2KQ.localdomain>
2020-11-10 18:55:01 -0500

    improve some feactures app

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
+import { useParams } from 'react-router-dom'
 
 const GET_MOVIES = gql`
   {
@@ -22,34 +23,28 @@ const GET_MOVIES = gql`
 `
 
 const Movies = (props) => {
-  // const [moviesIn, setMoviesIn] = useState([])
+  const { id } = useParams()
+  console.log('TLC: Movies -> id', id)
   const { loading, error, data } = useQuery(GET_MOVIES)
   if (loading) return <p>loading Messages...</p>
   if (error) {
     return <p>Error</p>
   }
 
-  // const getMovies = () => {
-  //   if (props.match.params.id) {
-  //     const movie = data.allFilms.films.filter((film) => {
-  //       return film.id === props.match.params.id
-  //     })
-  //     setMoviesIn(movie)
-  //     console.log('TLC: getMovies -> movie', movie)
-  //   }
-  // }
-  // getMovies()
+  const movie = data.allFilms.films.filter((film) => {
+    return film.id === id
+  })
 
   return (
     <div className='row justify-content-center'>
       <div className='col-md-10 '>
-        {data.allFilms.films.map(({ id, title, openingCrawl, director, planetConnection }) => (
+        {movie.map(({ id, title, openingCrawl, director, planetConnection }) => (
           <div key={id} className='card m-2'>
             <div className='card-body'>
-              <h2>{title}</h2>
-              <h6>{director}</h6>
-              <p>{openingCrawl}</p>
-              <div class='dropdown-divider'></div>
+              <h2 key={title}>{title}</h2>
+              <h6 key={director}>{director}</h6>
+              <p key={openingCrawl}>{openingCrawl}</p>
+              <div className='dropdown-divider'></div>
               <ul>
                 {planetConnection.planets.map(({ id, name }) => (
                   <li key={id}>{name}</li>
